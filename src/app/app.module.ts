45b15591c2b1a44004bebc7c95f3bfdeff33e1c4
Misterjo <plarocque.joel@gmail.com>
2023-06-21 16:26:53 -0400

    Finished basic layout and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './home/home/home.component';
+import { BoardComponent } from './game/board/board.component';
+import { SquareComponent } from './game/square/square.component';
+import { HistoryComponent } from './game/history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BoardComponent,
+    SquareComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,

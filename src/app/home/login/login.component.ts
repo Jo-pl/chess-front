4196d00cbbc5d5e815629e4fcab187fc149cb3b8
Misterjo <plarocque.joel@gmail.com>
2023-07-01 00:10:47 -0400

    Implemented form validation

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Auth } from 'aws-amplify';
 import { AuthService } from 'src/app/services/auth.service';
-import { FormBuilder } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,37 +12,89 @@ export class LoginComponent implements OnInit {
   router:Router;
   authService:AuthService;
   formBuilder:FormBuilder;
-  loginRegisterForm:any;
-  userEmail:string='';
-  buttonText:string='login';
+  loginRegisterForm:FormGroup;
+  userEmail:string = '';
+  formType:string = 'login';
+  loading:boolean = false;
+  errorMessage:string|null = null;
+  errorsActivated:boolean = false;
 
   constructor(private r: Router, private auth:AuthService, formBuilder: FormBuilder,) {
     this.router = r;
     this.authService = auth;
     this.formBuilder = formBuilder;
     this.loginRegisterForm = this.formBuilder.group({
-      email: '',
-      password: '',
-      code:''
+      email: ['', [Validators.required, Validators.email]],
+      password: ['',[Validators.required,Validators.minLength(6),Validators.maxLength(30)]],
+      code: ['',[Validators.required]]
     });
+  }
+
+  f(){
+    return this.loginRegisterForm.controls;
   }
 
   ngOnInit(): void {
     
   }
 
+  handleSubmit = async()=>{
+    if (this.loginRegisterForm.invalid) {
+      this.errorsActivated = true;
+      return;
+    }
+    
+    if(this.formType == 'login'){
+      await this.signIn();
+    }else if(this.formType == 'register'){
+      await this.signUp();
+      this.formType='confirm';
+    }else{
+      await this.verifyEmailValidationCode();
+    }
+  }
+
   signUp = async()=>{
-    console.log(this.loginRegisterForm.value['email']);
-    this.authService.signUp(this.loginRegisterForm.value['email'],this.loginRegisterForm.value['password']);
+    this.errorMessage = '';
+    let res = this.authService.signUp(this.loginRegisterForm.value['email'],this.loginRegisterForm.value['password']);
+    this.loading = true;
+    res.then((val)=>{
+      if(val){
+        if(val[1] == 'error'){
+          this.errorMessage = 'This code is either incorrect or expired, please try again';
+        }
+      }
+      this.loading = false;
+    });
     this.userEmail = this.loginRegisterForm.value['email'];
   }
 
   signIn = async()=>{
-    this.authService.signIn(this.loginRegisterForm.value['email'],this.loginRegisterForm.value['password']);
+    this.errorMessage = '';
+    let res = this.authService.signIn(this.loginRegisterForm.value['email'],this.loginRegisterForm.value['password']);
+    this.loading = true;
+    res.then((val)=>{
+      if(val){
+        if(val[1] == 'error'){
+          this.errorMessage = 'Incorrect user or password, please try again';
+        }
+      }
+      this.loading = false;
+      //this.router.navigate(['/']);
+    });
   }
 
   verifyEmailValidationCode = async()=>{
-    this.authService.verifyEmailValidationCode(this.userEmail,this.loginRegisterForm.value['code']);
+    this.errorMessage = '';
+    let res = this.authService.verifyEmailValidationCode(this.userEmail,this.loginRegisterForm.value['code']);
+    this.loading = true;
+    res.then((val)=>{
+      if(val){
+        if(val[1] == 'error'){
+          this.errorMessage = 'This code is either incorrect or expired, please try again';
+        }
+      }
+      this.loading = false;
+    });
   }
-
 }
--- src/app/shared/form-input/form-input.component.ts
+++ src/app/shared/form-input/form-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-input',
@@ -8,6 +9,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class FormInputComponent implements OnInit {
   @Input() placeholder!: string;
   @Input() icon!: string;
+  @Input() form!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
